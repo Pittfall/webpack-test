1110d3dcb17401011ad12b85c118d389f5d353d8
arusu <arusu@sehcomp.ca>
2018-10-09 11:28:31 -0400

    Added babel and configured it to compile next gen javascript to current version

--- webpack.config.js
+++ webpack.config.js
@@ -10,5 +10,17 @@ module.exports = {
       path: path.resolve(__dirname, 'dist'), // Where output should be stored.
       filename: 'bundle.js', // The javascript files are bundled into this file.
       publicPath: '' // Empty string means root folder meaning the file structure we have here is the output/deployed file structure.
+   },
+   resolve: {
+      extensions: ['.js', '.jsx'] // This will try to auto resolve imports of this file type without needing to type them in.
+   },
+   module: {  // What webpack should do with each file.
+      rules: [
+         {
+            test: /\.js$/, // Test if a file fulfils criteria. ex. Ends with .js.
+            loader: 'babel-loader', // The package that does the work.
+            exclude: /node_modules/ // Don't transform anything in node modules because they have already been optimized and transformed.
+         }
+      ]
    }
 };
